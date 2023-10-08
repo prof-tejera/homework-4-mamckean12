ee69df492ce922a5dabfba169ca4f9db88837d53
mamckean12 <mamckean@gmail.com>
2023-10-08 16:50:52 -0400

    conditionals for each operator type

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,16 +6,30 @@ import Screen from "./Screen";
 const Calculator = () => {
   /** TODO: Here is where you are going to keep track of calculator state */
   const [count, setCount] = useState(1);
+  const [num, setNum] = useState("");
+  const [operator, setOperator] = useState("");
 
   /** TODO: what happens when I click a number? */
   const handleNumberClick = (e) => {
     console.log("number clicked: ", e.target.innerText);
+    setNum((previousState) => previousState + e.target.innerText);
+    console.log("num state: ", num);
   };
 
   /** TODO: what happens when I click an operation? */
   const handleOperationClick = (e) => {
     console.log("operator clicked: ", e.target.innerText);
-    
+    // change non "+" operators to "+" (because only one operator required for this homework) 
+    if (e.target.innerText === "-" || e.target.innerText === "/" || e.target.innerText === "x") {
+      setOperator("+");
+      console.log("operator: ", operator)
+    } else if (e.target.innerText === "=") {
+      setOperator(e.target.innerText);
+      console.log("operator: ", operator);
+    } else {
+      setOperator(e.target.innerText);
+      console.log("operator: ", operator);
+    }
   };
 
   return (
